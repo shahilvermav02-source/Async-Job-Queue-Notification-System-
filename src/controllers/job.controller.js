55e2dccfded010f43627e5dea249f6366eaeb76e
Shahil Verma <shahil.vermav02@gmail.com>
2026-06-17 14:07:17 +0545

    improvement' '

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -1,16 +1,28 @@
 import {notificationQueue} from "../queue.js"
 import { ApiResponse } from "../utils/api_response.js"
 const welcome_emails= async(req,res)=>{
+     if (!req.body.to || !req.body.subject) {
+       throw new ApiError(400, "Email and subject are required");
+    }
     const job = await notificationQueue.add(
-        "Send-emails",
+        "Testing-mail",
         {
             to:req.body.to,
             subject:req.body.subject,
             text:"the is the welcome by shahil verma , I am learnign the bull mq for better api response",
+        },
+        {
+         attempts: 3,
+         backoff: {
+         type: "fixed",
+         delay: 5000
+            }
         }
     )
     res.status(201).json(
-        new ApiResponse(201,null,"successfully add the job")
+        new ApiResponse(201,{
+            jobId:job.id
+        },"successfully add the job")
     )
 }
 
--- src/services/mail.services.js
+++ src/services/mail.services.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAILTRAP_HOST,
-  port: process.env.MAILTRAP_PORT,
+  port: Number(process.env.MAILTRAP_PORT),
   auth: {
     user: process.env.MAILTRAP_USER,
     pass: process.env.MAILTRAP_PASS,
